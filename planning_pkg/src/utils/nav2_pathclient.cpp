9cab16d4a6ee35ff7208c4b196e05befa9f1c365
Ruben_Malacarne <ruben.malacarne@gmail.com>
2025-08-17 12:32:43 +0200

    update nav2, now is more slim

--- planning_pkg/src/utils/nav2_pathclient.cpp
+++ planning_pkg/src/utils/nav2_pathclient.cpp
@@ -20,21 +20,25 @@ class PathPublisher : public rclcpp::Node
 public:
   PathPublisher() : Node("Nav2Client")
   {
-    // Action clients (nomi come in Nav2 multi-robot)
+    // Action clients 
     client1_ptr_ = rclcpp_action::create_client<FollowPath>(this, "/shelfino1/follow_path");
     client2_ptr_ = rclcpp_action::create_client<FollowPath>(this, "/shelfino2/follow_path");
 
-    // Sottoscrizioni ai topic path del planner (quelli pubblicati dal tuo PathPlanningOrchestratorClient)
+   
     auto qos = rclcpp::QoS(rclcpp::KeepLast(1));
-    _path_subscription1 = this->create_subscription<nav_msgs::msg::Path>(
-        "/path_pos1_to_gates", qos,
-        std::bind(&PathPublisher::store_path1, this, std::placeholders::_1));
+      _path_subscription1 = this->create_subscription<nav_msgs::msg::Path>(
+    "/path_1_smoothed", qos,
+    [this, client = client1_ptr_](nav_msgs::msg::Path::ConstSharedPtr msg) {
+      this->store_path(*msg, client, "shelfino1");
+    });
 
     _path_subscription2 = this->create_subscription<nav_msgs::msg::Path>(
-        "/path_pos2_to_gates", qos,
-        std::bind(&PathPublisher::store_path2, this, std::placeholders::_1));
+    "/path_pos2_to_gates", qos,
+    [this, client = client2_ptr_](nav_msgs::msg::Path::ConstSharedPtr msg) {
+      this->store_path(*msg, client, "shelfino2");
+    });
 
-    // Attendi i due action server (con timeout per non bloccare in eterno)
+    
     if (!client1_ptr_->wait_for_action_server(std::chrono::seconds(10))) {
       RCLCPP_ERROR(this->get_logger(), "Action server /shelfino1/follow_path non disponibile.");
     } else {
@@ -52,7 +56,6 @@ public:
 
 private:
 
-  // ===== Helpers =====
   void send_path_goal_(const rclcpp_action::Client<FollowPath>::SharedPtr &client,
                        const nav_msgs::msg::Path &path,
                        const std::string &who)
@@ -101,26 +104,18 @@ private:
     client->async_send_goal(goal, opts);
   }
 
-  // ===== Callbacks =====
-  void store_path1(const nav_msgs::msg::Path &msg)
+  void store_path(const nav_msgs::msg::Path &msg, 
+                  const rclcpp_action::Client<FollowPath>::SharedPtr &client,
+                  const std::string &robot_name)
   {
-    full_path1 = msg;
+    nav_msgs::msg::Path path = msg;
     // Assicura header coerente (se vuoi forzare)
-    full_path1.header.stamp = this->get_clock()->now();
-    if (full_path1.header.frame_id.empty()) full_path1.header.frame_id = "map";
+    path.header.stamp = this->get_clock()->now();
+    if (path.header.frame_id.empty()) path.header.frame_id = "map";
 
-    RCLCPP_INFO(this->get_logger(), "Path 1 ricevuto: %zu poses. Invio a shelfino1...", full_path1.poses.size());
-    send_path_goal_(client1_ptr_, full_path1, "shelfino1");
-  }
-
-  void store_path2(const nav_msgs::msg::Path &msg)
-  {
-    full_path2 = msg;
-    full_path2.header.stamp = this->get_clock()->now();
-    if (full_path2.header.frame_id.empty()) full_path2.header.frame_id = "map";
-
-    RCLCPP_INFO(this->get_logger(), "Path 2 ricevuto: %zu poses. Invio a shelfino2...", full_path2.poses.size());
-    send_path_goal_(client2_ptr_, full_path2, "shelfino2");
+    RCLCPP_INFO(this->get_logger(), "Path ricevuto per %s: %zu poses. Invio...", 
+                robot_name.c_str(), path.poses.size());
+    send_path_goal_(client, path, robot_name);
   }
 
   // ===== Membri =====
@@ -129,9 +124,6 @@ private:
 
   rclcpp_action::Client<FollowPath>::SharedPtr client1_ptr_;
   rclcpp_action::Client<FollowPath>::SharedPtr client2_ptr_;
-
-  nav_msgs::msg::Path full_path1;
-  nav_msgs::msg::Path full_path2;
 };
 
 int main(int argc, char *argv[])
