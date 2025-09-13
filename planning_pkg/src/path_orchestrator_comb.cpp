fb79029c2833fef5421442c2e180e59b4aee9fb2
Ruben_Malacarne <ruben.malacarne@gmail.com>
2025-09-13 12:22:12 +0200

    add readme

--- planning_pkg/src/path_orchestrator_comb.cpp
+++ planning_pkg/src/path_orchestrator_comb.cpp
@@ -323,8 +323,8 @@ private:
         bool is_linear_1 = linear_gen_.is_direct_path_feasible(wps_pos1.front(), wps_pos1.back(), last_obstacles_, last_arena_);
         bool is_linear_2 = linear_gen_.is_direct_path_feasible(wps_pos2.front(), wps_pos2.back(), last_obstacles_, last_arena_);
         
-        is_linear_1 = false; // FORZATURA PER TESTING COMB
-        is_linear_2 = false; // FORZATURA PER TESTING COMB
+        // is_linear_1 = false; // FORZATURA PER TESTING COMB
+        // is_linear_2 = false; // FORZATURA PER TESTING COMB
         
         if (is_linear_1)
         {
